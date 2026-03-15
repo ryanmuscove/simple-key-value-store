546214f776437932fd5c0e09883f8dfdaed248aa
Ryan Muscove <ryanmuscove@my.unt.edu>
2026-03-15 16:21:26 -0500

    setting up the last write

--- Desktop/project/kvstore.py
+++ Desktop/project/kvstore.py
@@ -8,21 +8,24 @@ while True:
         split = user_input.split() #splitting 
         command = split[0]
         if command.lower() == "set":
+            found = False
             key = split[1]
             value = split[2]
-            print(key,  value)
-            database.append([key, value])
+            for pair in database:
+               if pair[0] == split[1]: #pair key matching request key
+                  pair[1] = value  #makes new value
+                  found = True
+               else:
+                    database.append([key, value])
       
         elif command.lower() == "get": #get command
          key = split[1]
-         for pair in database:
-            pair[0] = key
-            pair[1]
-            if pair == key:
-               print(value)
+         for pair in database: #the linear seach
+            if pair[0] == key:
+               print(pair[1])
          
 
-print(database)
+
 
 
 
